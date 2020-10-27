bbd7a948e47842170b567108a46511e95cf7772f
Brendan Hagan <hagan@stsci.edu>
2020-10-27 15:16:02 -0400

    Update test_db.py

--- miri_pixel_db_code/tests/test_db.py
+++ miri_pixel_db_code/tests/test_db.py
@@ -29,7 +29,7 @@ def test_db_unit():
 
     script_path = find_script(pyscript,os.path.dirname(os.getcwd()))
     #exposure_path = 'exposures/'+orig_exp
-    exposure_path ='/home/travis/build/STScI-MIRI/MIRI-Pixel-DB/miri_pixel_db_code/exposures/'+orig_exp
+    exposure_path ='/home/travis/build/STScI-MIRI/MIRI-Pixel-DB/miri_pixel_db_code/tests/exposures/'+orig_exp
     run_cmd = ['python',script_path,'test',exposure_path,'None',connection_string]
     print("Script Path:",script_path)
     print("Command Line cmd:",' '.join(run_cmd))
